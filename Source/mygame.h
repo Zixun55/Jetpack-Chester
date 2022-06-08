6983c087a08b6aa6487f5d868cf6fdc88fca7f96
S1nceU <jjkk900925@gmail.com>
2022-06-08 16:48:57 +0800

    some_del

--- Source/mygame.h
+++ Source/mygame.h
@@ -38,9 +38,7 @@
  *      3. Use ShowInitProgress(percent) to display loading progress.
 */
 
-#include "CEraser.h"
-#include "CBall.h"
-#include "CBouncingBall.h"
+
 #include "Ccharacter.h"
 #include "CMap.h"
 #include "CBlock.h"
